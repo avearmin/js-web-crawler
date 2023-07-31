d7ba976b65d6a2d426a23e95c9dc86948be0bb97
Armin Radoncic <Arminradoncic1@gmail.com>
2023-07-31 16:40:28 -0400

    Add more test cases for normalizeURL method

--- crawl.test.js
+++ crawl.test.js
@@ -2,13 +2,25 @@ const { test, expect } = require('@jest/globals')
 const { normalizeURL } = require('./crawl.js')
 
 test('normalize http://GooGle.COM', () => {
-    expect(normalizeURL('http://GooGle.COM')).toBe('google.com')
+    expect(normalizeURL('http://GooGle.COM')).toBe('google.com/')
 })
 
 test('normalize https://gooGle.COM', () => {
-    expect(normalizeURL('http://gooGle.COM')).toBe('google.com')
+    expect(normalizeURL('http://gooGle.COM')).toBe('google.com/')
 })
 
 test('normalize https://GOOGLE.COM', () => {
-    expect(normalizeURL('https://GOOGLE.COM')).toBe('google.com')
+    expect(normalizeURL('https://GOOGLE.COM')).toBe('google.com/')
+})
+
+test('normalize https://SomeWebSite.com/some/path', () => {
+    expect(normalizeURL('https://SomeWebSite.com/some/path')).toBe('somewebsite.com/some/path')
+})
+
+test('normalize https://SomeWebSite.com/some/path/', () => {
+    expect(normalizeURL('https://SomeWebSite.com/some/path/')).toBe('somewebsite.com/some/path')
+})
+
+test('normalize https://SomeWebSite.com/some/path///////////', () => {
+    expect(normalizeURL('https://SomeWebSite.com/some/path///////////')).toBe('somewebsite.com/some/path')
 })
