864bb4b20eb85ba8fe453f6c768a7ea25569687c
Armin Radoncic <Arminradoncic1@gmail.com>
2023-07-31 16:58:26 -0400

    Implement normalizeURL function
    The normalizeURL function is now implemented to handle URL normalization,
    and the removeAllTrailingSlashes method is included to remove any trailing
    slashes from the end of the pathname.

--- crawl.js
+++ crawl.js
@@ -1,5 +1,25 @@
-function normalizeURL(url) {
+function removeAllTrailingSlashes(str) {
+    if (str.length === 1) { // Handles the case where the str is a single trailing slash
+        return str
+    }
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (!(str[i] === '/')) {
+            return str.substring(0, i + 1)
+        }
+    }
+}
 
+function normalizeURL(url) {
+    const urlObj = new URL(url)
+    let normalizedURL = ''
+    if (urlObj.host) {
+        normalizedURL += urlObj.host
+    }
+    if (urlObj.pathname) {
+        normalizedPathname = removeAllTrailingSlashes(urlObj.pathname)
+        normalizedURL += normalizedPathname
+    }
+    return normalizedURL
 }
 
 module.exports = {
